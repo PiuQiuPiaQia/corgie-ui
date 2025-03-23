f9f896bab832504dd89099f55c2d0af0ac1a6aa1
allen <329988300@qq.com>
2025-03-23 22:09:01 +0800

    fix: 启动报错

--- config-overrides.js
+++ config-overrides.js
@@ -1,17 +1,32 @@
 module.exports = function override(config, env) {
     // config.output.filename = 'static/js/[hash].bundle.js';
+    
+    // 修改 babel-loader 配置，排除 bitset.min.js
+    const babelLoaderRule = config.module.rules
+        .find(rule => rule.oneOf)
+        .oneOf.find(rule => rule.loader && rule.loader.includes('babel-loader'));
+    
+    if (babelLoaderRule) {
+        babelLoaderRule.exclude = /bitset\.min\.js$/;
+    }
+
+    // 添加对 bitset.min.js 的特殊处理
+    config.module.rules.push({
+        test: /bitset\.min\.js$/,
+        use: {
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'static/js/'
+            }
+        }
+    });
+
     config.module.rules.push({
         test: /\.worker\.js$/,
         loader: "worker-loader",
         options: {
             filename: '[name].worker.js'
-            // filename: (pathData) => {
-            //     // if (/\.worker\.(c|m)?js$/i.test(pathData.chunk.entryModule.resource)) {
-            //     //     return "[name].worker.js";
-            //     // }
-
-            //     return "[name].js";
-            // },
         },
     });
     return config;
